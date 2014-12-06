cb58032af2edde995fc66da3e07cc8b56c676e55
fmondaca <f.mondaca@uni-koeln.de>
2014-12-06 02:20:52 +0100

    arc - maven structure, minor cleaning

--- arc.backend/arc.extractions/nvs/nvs.parser/src/test/java/de/uni_koeln/spinfo/arc/parser/NVSParserTest.java
+++ arc.backend/arc.extractions/nvs/nvs.parser/src/test/java/de/uni_koeln/spinfo/arc/parser/NVSParserTest.java
@@ -4,7 +4,7 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
-import de.uni_koeln.spinfo.arc.common.DictUtils;
+import de.uni_koeln.spinfo.antlr.nvs.common.DictUtils;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
