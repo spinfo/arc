c7a6a29158f92a386ac834d536ef284ca12e6669
fmondaca <f.mondaca@uni-koeln.de>
2015-04-24 12:13:52 +0200

    up

--- arc.ext.eichenhofer.pdfbox/src/main/java/de/uni_koeln/spinfo/arc/eichenhofer/pdfbox/ExtractEichenhofer.java
+++ arc.ext.eichenhofer.pdfbox/src/main/java/de/uni_koeln/spinfo/arc/eichenhofer/pdfbox/ExtractEichenhofer.java
@@ -5,111 +5,113 @@ import com.itextpdf.text.pdf.parser.LocationTextExtractionStrategy;
 import com.itextpdf.text.pdf.parser.PdfReaderContentParser;
 import com.itextpdf.text.pdf.parser.RenderFilter;
 import com.itextpdf.text.pdf.parser.TextExtractionStrategy;
+import com.snowtide.pdf.OutputTarget;
+import com.snowtide.pdf.PDFTextStream;
+import de.uni_koeln.spinfo.arc.utils.FileUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.util.PDFTextStripper;
 
 import java.io.*;
-import java.util.List;
 
 /**
  * Created by franciscomondaca on 6/11/14.
  */
 public class ExtractEichenhofer {
 
-	public static String eich_input = "../eichenhofer/eichenhofer.data/20140715_eichenhofer_ocr.pdf";
+    public static String eich_input = "../eichenhofer/eichenhofer.data/20140715_eichenhofer_ocr.pdf";
 
-	public void extractWithPDFBOX(String outputFile) throws IOException {
+    public void extractWithPDFBOX(String outputFile) throws IOException {
 
-		PDDocument pd;
-		BufferedWriter wr;
-		try {
-			File input = new File(eich_input);
-			File output = new File("../antlr4.parent/eichenhofer.data/"
-					+ outputFile + "txt");
-			pd = PDDocument.load(input);
-			System.out.println(pd.getNumberOfPages());
-			System.out.println(pd.isEncrypted());
+        PDDocument pd;
+        BufferedWriter wr;
+        try {
+            File input = new File(eich_input);
+            File output = new File("../antlr4.parent/eichenhofer.data/"
+                    + outputFile + "txt");
+            pd = PDDocument.load(input);
+            System.out.println(pd.getNumberOfPages());
+            System.out.println(pd.isEncrypted());
 
-			PDFTextStripper stripper = new PDFTextStripper();
+            PDFTextStripper stripper = new PDFTextStripper();
 
-			wr = new BufferedWriter(new OutputStreamWriter(
-					new FileOutputStream(output)));
+            wr = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(output)));
 
-			stripper.writeText(pd, wr);
-			if (pd != null) {
-				pd.close();
-			}
+            stripper.writeText(pd, wr);
+            if (pd != null) {
+                pd.close();
+            }
 
-			wr.close();
+            wr.close();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void extractWithIText(String outputFile) throws IOException {
-		PdfReader reader = new PdfReader(eich_input);
-		PdfReaderContentParser parser = new PdfReaderContentParser(reader);
-		PrintWriter out = new PrintWriter(new FileOutputStream(
-				"../antlr4.parent/eichenhofer.data/" + outputFile + "txt"));
-		TextExtractionStrategy strategy;
+    public void extractWithIText(String outputFile) throws IOException {
+        PdfReader reader = new PdfReader(eich_input);
+        PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+        PrintWriter out = new PrintWriter(new FileOutputStream(
+                "../antlr4.parent/eichenhofer.data/" + outputFile + "txt"));
+        TextExtractionStrategy strategy;
 
-		RenderFilter filter;
+        RenderFilter filter;
 
-		for (int i = 1; i <= reader.getNumberOfPages(); i++) {
-			// strategy = parser.processContent(i, new
-			// SimpleTextExtractionStrategy());
-			strategy = parser.processContent(i,
-					new LocationTextExtractionStrategy());
+        for (int i = 1; i <= reader.getNumberOfPages(); i++) {
+            // strategy = parser.processContent(i, new
+            // SimpleTextExtractionStrategy());
+            strategy = parser.processContent(i,
+                    new LocationTextExtractionStrategy());
 
-			out.println(strategy.getResultantText());
-		}
-		out.flush();
-		out.close();
-		reader.close();
+            out.println(strategy.getResultantText());
+        }
+        out.flush();
+        out.close();
+        reader.close();
 
-	}
+    }
 
-//	public void extractWithPDFTextStream(String outputFile) throws FileNotFoundException {
-//
-//		String pdfFilePath = eich_input;
-//		PDFTextStream pdfts = new PDFTextStream(pdfFilePath);
-//		StringBuilder text = new StringBuilder(1024);
-//		pdfts.pipe(new OutputTarget(text));
-//		pdfts.close();
-//
-//		System.out.printf("The text extracted from %s is:", pdfFilePath);
-//		System.out.println(text);
-//		PrintWriter out = new PrintWriter(new FileOutputStream(
-//				"../eichenhofer/eichenhofer.data/" + outputFile + ".txt"));
-//
-//		out.print(text);
-//
-//		out.flush();
-//		out.close();
-//
-//	}
+    public void extractWithPDFTextStream(String outputFile) throws FileNotFoundException {
 
-	// public void extractBookmarks() throws IOException {
-	//
-	//
-	// PDFTextStream pdfts = new PDFTextStream(eich_input);
-	// Bookmark root = pdfts.getBookmarks();
-	// if (root == null) {
-	// System.out.printf("%s does not contain any bookmarks.", eich_input);
-	// } else {
-	// for (Bookmark b : (List<Bookmark>) root.getAllDescendants()) {
-	// System.out.printf("Bookmark '%s' points at page %s, bounds %s, %s, %s, %s",
-	// b.getTitle(), b.getPageNumber(),
-	// b.getLeftBound(), b.getBottomBound(),
-	// b.getRightBound(), b.getTopBound());
-	// System.out.println();
-	// }
-	// }
-	//
-	// pdfts.close();
-	//
-	//
-	// }
+        String pdfFilePath = FileUtils.inputPath + "tscharner-20140715_20140923-20150318.pdf";
+        PDFTextStream pdfts = new PDFTextStream(pdfFilePath);
+        StringBuilder text = new StringBuilder(1024);
+        pdfts.pipe(new OutputTarget(text));
+        pdfts.close();
+
+        System.out.printf("The text extracted from %s is:", pdfFilePath);
+        System.out.println(text);
+        PrintWriter out = new PrintWriter(new FileOutputStream(
+                FileUtils.outputPath + outputFile + ".txt"));
+
+        out.print(text);
+
+        out.flush();
+        out.close();
+
+    }
+
+    // public void extractBookmarks() throws IOException {
+    //
+    //
+    // PDFTextStream pdfts = new PDFTextStream(eich_input);
+    // Bookmark root = pdfts.getBookmarks();
+    // if (root == null) {
+    // System.out.printf("%s does not contain any bookmarks.", eich_input);
+    // } else {
+    // for (Bookmark b : (List<Bookmark>) root.getAllDescendants()) {
+    // System.out.printf("Bookmark '%s' points at page %s, bounds %s, %s, %s, %s",
+    // b.getTitle(), b.getPageNumber(),
+    // b.getLeftBound(), b.getBottomBound(),
+    // b.getRightBound(), b.getTopBound());
+    // System.out.println();
+    // }
+    // }
+    //
+    // pdfts.close();
+    //
+    //
+    // }
 
 }
